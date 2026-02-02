7701413b032244a086883517f9bf9e1e2bfea9a0
Abdodib <abdelalim.dib@etu.usthb.dz>
2026-02-02 20:13:17 +0100

    feat: add JWT token generation utility and update dependencies

---
+++ src/util/generatortoken.ts
@@ -0,0 +1,18 @@
+import jwt, { SignOptions } from "jsonwebtoken";
+
+const generateToken = (userId: string): string => {
+  if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET is not defined in .env");
+  }
+
+  const payload = { id: userId };
+
+  const options: SignOptions = {
+    expiresIn: process.env.JWT_EXPIRES_IN ? parseInt(process.env.JWT_EXPIRES_IN) : 604800,
+  };
+
+  const token = jwt.sign(payload, process.env.JWT_SECRET, options);
+  return token;
+};
+
+export default generateToken;
